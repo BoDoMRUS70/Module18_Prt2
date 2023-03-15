466bbabba7615450ce983fa90de9c1ea58876386
Bodomrus70@mail.ru <Bodomrus70@mail.ru>
2023-03-15 16:25:13 +0700

    Добавьте файлы проекта.

---
+++ Module18_Prt2/Module18_Patterns/DownloadVideoCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18_Prt2
+{
+
+    class DownloadVideoCommand : ICommand
+    {
+        VideoPlayer videoPlayer;
+        public DownloadVideoCommand(VideoPlayer videoPlayer)
+        {
+            this.videoPlayer = videoPlayer;
+        }
+        public async Task ExecuteAsync()
+        {
+            await videoPlayer.DownloadVideoAsync();
+        }
+    }
+}
+
---
+++ Module18_Prt2/Module18_Patterns/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+
+namespace Module18_Prt2
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            
+            var sender = new Sender();
+            
+            var videoPlayer = new VideoPlayer();
+           
+            Console.WriteLine("Enter video Url:");
+            videoPlayer.videoUrl = Console.ReadLine();
+
+            var getVideoInfoCommand = new GetVideoInfoCommand(videoPlayer);
+            var downloadVideoCommand = new DownloadVideoCommand(videoPlayer);
+           
+            sender.SetCommand(getVideoInfoCommand);
+            
+            await sender.Run();
+            
+            sender.SetCommand(downloadVideoCommand);
+           
+            await sender.Run();
+            
+        }
+    }
+}
